483905d24ea093df9e37635e1f48a80d62f24926
kungurtsev <kungurtsev@skbkontur.ru>
2018-07-28 11:04:16 +0500

    controller

---
+++ hello/Controllers/AliceController.cs
@@ -0,0 +1,14 @@
+﻿using System.Web.Http;
+using hello.lib;
+
+namespace hello.Controllers
+{
+    public class AliceController : ApiController
+    {
+        [Route("alice"), HttpPost]
+        public AliceResponse Post([FromBody] AliceRequest req)
+        {
+            return req.Reply("Привет от Контура");
+        }
+    }
+}
